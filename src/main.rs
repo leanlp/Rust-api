9dae4f2896265c4357fd34822a332a4862959448
leanlp <0xleansan@gmail.com>
2024-05-13 09:25:58 -0300

    using ethers-rs to fetch data from erc721

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use ethers::prelude::*;
+use ethers::providers::{Provider, Http};
+use std::sync::Arc;
+use std::env;
+use dotenv::dotenv;
+//use log::{info, error};
 use serde_json::Value;
-use reqwest::{Error as ReqwestError, Response};
-use std::time::Duration;
+use tokio::time::Duration;
+use reqwest::Response;
 
 async fn fetch_eth_usdt_price() -> Result<f64, Box<dyn std::error::Error>> {
     let url = "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT";
@@ -21,14 +27,28 @@ async fn parse_eth_usdt_price(response: Response) -> Result<f64, Box<dyn std::er
     Err("Failed to parse response".into())
 }
 
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    //env_logger::init();
+
+    let contract_address = "0x3D18C60D612Dd0c0096aA83007dBa0B7F66a2418".parse::<Address>()?;
+    abigen!(IERC721, "./src/abi.json");
+
+    let rpc_url = env::var("SEPOLIA_RPC_URL").expect("RPC_URL not set in .env file");
+    let provider = Provider::<Http>::try_from(rpc_url)?;
+    let provider = Arc::new(provider);
+    let contract = IERC721::new(contract_address, provider.clone());
+
+    let result: String = contract.method("symbol", ())?.call().await?;
+    println!("Contract symbol: {}", result);
+
     loop {
         match fetch_eth_usdt_price().await {
             Ok(price) => println!("ETH/USDT Price: {}", price),
             Err(e) => eprintln!("Error: {}", e),
         }
-        // Wait for 5 seconds before fetching again
         tokio::time::sleep(Duration::from_secs(5)).await;
     }
 }
